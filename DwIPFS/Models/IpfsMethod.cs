315e504a31e6793e1ad015302a7b560ca83ac31c
DwGoing <jianghy1209@163.com>
2020-03-13 01:22:18 +0800

    新增接口

--- DwIPFS/Models/IpfsMethod.cs
+++ DwIPFS/Models/IpfsMethod.cs
@@ -5,6 +5,28 @@ namespace DwIPFS
     public class IpfsMethod
     {
         public const string Add = "add";
+        public const string BitswapLedger = "bitswap/ledger";
+        public const string BitswapReprovide = "bitswap/reprovide";
+        public const string BitswapStat = "bitswap/stat";
+        public const string BitswapWantList = "bitswap/wantlist";
+        public const string BlockGet = "block/get";
+        public const string BlockPut = "block/put";
+        public const string BlockRm = "block/rm";
+        public const string BlockStat = "block/stat";
+        public const string Bootstrap = "bootstrap";
+        public const string BootstrapAdd = "bootstrap/add";
+        public const string BootstrapAddDefault = "bootstrap/add/default";
+        public const string BootstrapList = "bootstrap/list";
+        public const string BootstrapRm = "bootstrap/rm";
+        public const string BootstrapRmAll = "bootstrap/rm/all";
+        public const string CidBase32 = "cid/base32";
+        public const string CidBases = "cid/bases";
+        public const string CidCodecs = "cid/codecs";
+        public const string CidFormat = "cid/format";
+        public const string CidHashes = "cid/hashes";
+        public const string Commands = "commands";
+        public const string Config = "config";
+        public const string ConfigEdit = "config/edit";
         public const string Cat = "cat";
     }
 }
--- DwIPFS/Models/IpfsStructure.cs
+++ DwIPFS/Models/IpfsStructure.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DwIPFS
 {
@@ -9,8 +10,129 @@ namespace DwIPFS
         public int Size { get; set; }
     }
 
-    public class CatResult
+    public class BitswapLedgerResult
     {
+        public string Peer { get; set; }
+        public int Value { get; set; }
+        public int Sent { get; set; }
+        public int Recv { get; set; }
+        public int Exchanged { get; set; }
+    }
 
+    public class BitswapStatResult
+    {
+        public int ProvideBufLen { get; set; }
+        public string[] Wantlist { get; set; }
+        public string[] Peers { get; set; }
+        public int BlocksReceived { get; set; }
+        public int DataReceived { get; set; }
+        public int BlocksSent { get; set; }
+        public int DataSent { get; set; }
+        public int DupBlksReceived { get; set; }
+        public int DupDataReceived { get; set; }
+        public int MessagesReceived { get; set; }
+    }
+
+    public class BitswapWantListResult
+    {
+        public KeyValuePair<string, string>[] Keys { get; set; }
+    }
+
+    public class BlockPutResult
+    {
+        public string Key { get; set; }
+        public int Size { get; set; }
+    }
+
+    public class BlockRmResult
+    {
+        public string Error { get; set; }
+        public string Hash { get; set; }
+    }
+
+    public class BlockStatResult
+    {
+        public string Key { get; set; }
+        public int Size { get; set; }
+    }
+
+    public class BootstrapResult
+    {
+        public string[] Peers { get; set; }
+    }
+
+    public class BootstrapAddResult
+    {
+        public string[] Peers { get; set; }
+    }
+
+    public class BootstrapAddDefaultResult
+    {
+        public string[] Peers { get; set; }
+    }
+
+    public class BootstrapListResult
+    {
+        public string[] Peers { get; set; }
+    }
+
+    public class BootstrapRmResult
+    {
+        public string[] Peers { get; set; }
+    }
+
+    public class BootstrapRmAllResult
+    {
+        public string[] Peers { get; set; }
+    }
+
+    public class CidBase32Result
+    {
+        public string CidStr { get; set; }
+        public string ErrorMsg { get; set; }
+        public string Formatted { get; set; }
+    }
+
+    public class BaseEncoding
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class CidCodec
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class CidFormatResult
+    {
+        public string CidStr { get; set; }
+        public string ErrorMsg { get; set; }
+        public string Formatted { get; set; }
+    }
+
+    public class CidHash
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class CommandOption
+    {
+        public string[] Names { get; set; }
+    }
+
+    public class Command
+    {
+        public string Name { get; set; }
+        public CommandOption[] Options { get; set; }
+        public Command[] Subcommands { get; set; }
+    }
+
+    public class ConfigResult
+    {
+        public string Key { get; set; }
+        public object Value { get; set; }
     }
 }
